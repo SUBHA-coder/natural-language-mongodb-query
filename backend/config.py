910bfce580027a842c60f070297a86397191b6ec
SOUMYADEEPDUTTACODER <duttasoumyadeep594@gmail.com>
2025-10-31 02:23:57 +0530

    put config in gitignore

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,6 @@
 import os
 
-# Load variables from .env if python-dotenv is available
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except Exception:
-    pass
+
 
 # MongoDB settings
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
